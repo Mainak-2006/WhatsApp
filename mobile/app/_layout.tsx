3ef5638e770a01d1d7a9e9933e5d35c655581c3d
MAINAK KUNDU <mainakkundu13@gmail.com>
2026-01-29 20:54:50 +0530

    theme feature

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,20 +6,18 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import useColourScheme from '@/components/useColourScheme';
 
 
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -28,7 +26,6 @@ export default function RootLayout() {
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -47,10 +44,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
+  const { colorScheme } = useColourScheme();
 
   return (
-    <ThemeProvider value={DarkTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
